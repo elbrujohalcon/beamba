66fde4e2d54d41db72a1d7ff240cc06935e84acd
Brujo Benavides <fernando.benavides@adroll.com>
2018-11-03 18:30:09 -0300

    Plan the talk

--- beamba.ex
+++ beamba.ex
@@ -1,5 +1,6 @@
-defmodule BeamBA.Index do
-  alias __MODULE__
+defmodule BeamBA.Test do
+  alias BeamBA.Index
+  alias BeamBA.Uniq
 
   defp assert_up(input) do
     ^input = input |> Index.unpack() |> Index.pack()
@@ -9,7 +10,23 @@ defmodule BeamBA.Index do
     ^input = input |> Index.pack() |> Index.unpack()
   end
 
-  def test do
+  def run do
+    [
+      &test_uniq/0,
+      &test_pack/0
+    ]
+    |> Enum.each(& &1.())
+  end
+
+  defp test_uniq do
+    [] = Uniq.run([])
+    [1] = Uniq.run([1])
+    [1] = Uniq.run([1, 1, 1])
+    [1, 2] = Uniq.run([1, 2, 1, 1, 2])
+    [:a, :c, :b] = Uniq.run([:a, :a, :c, :b, :a, :c])
+  end
+
+  defp test_pack do
     %{} = Index.pack([])
     %{:a => [1]} = Index.pack([:a])
     %{:a => [1, 2, 3]} = Index.pack([:a, :a, :a])
@@ -37,7 +54,67 @@ defmodule BeamBA.Index do
     ]
     |> Enum.each(&assert_up/1)
   end
+end
 
+## This doesn't work because it doesn't preserve the order
+# defmodule BeamBA.Uniq do
+#   def run([]), do: []
+
+#   def run([elem | rest]) do
+#     rec = run(rest)
+#     if member?(rec, elem) do
+#       rec
+#     else
+#       [elem | rec]
+#     end
+#   end
+
+#   def member?([], _elem), do: false
+#   def member?([elem | _rest], elem), do: true
+#   def member?([_not_elem | rest], elem), do: member?(rest, elem)
+# end
+
+## This is the original version
+# defmodule BeamBA.Uniq do
+#   def run([]), do: []
+
+#   def run([elem | rest]) do
+#     [elem | run(remove(rest, elem))]
+#   end
+
+#   defp remove([], _elem), do: []
+#   defp remove([elem | rest], elem), do: remove(rest, elem)
+#   defp remove([not_elem | rest], elem), do: [not_elem | remove(rest, elem)]
+# end
+
+## This version doesn't use Index.map
+# defmodule BeamBA.Uniq do
+#   def run(list) do
+#     list
+#     |> BeamBA.Index.pack()
+#     |> Map.to_list()
+#     |> keep_first_pos
+#     |> Map.new()
+#     |> BeamBA.Index.unpack()
+#   end
+
+#   defp keep_first_pos([]), do: []
+#   defp keep_first_pos([{elem, [pos | _]} | rest]), do: [{elem, [pos]} | keep_first_pos(rest)]
+# end
+
+## This is the final version
+defmodule BeamBA.Uniq do
+  def run(list) do
+    list
+    |> BeamBA.Index.pack()
+    |> BeamBA.Index.map(&keep_first_pos/2)
+    |> BeamBA.Index.unpack()
+  end
+
+  defp keep_first_pos(elem, [pos | _]), do: [pos]
+end
+
+defmodule BeamBA.Index do
   @doc """
   This is the easiest form of recursion.
   The only trick is to have an auxiliary function.
@@ -56,29 +133,39 @@ defmodule BeamBA.Index do
   We also have an auxiliary function, but now have to traverse the index many times.
   This function should not be used for anything other than tests.
   The interesting recursion part is &key_for/2
+
+  We accept corrupt indexes (without some positions) to allow for special operations (see above)
   """
   def unpack(map), do: unpack(map, 1)
 
   defp unpack(index, _pos) when map_size(index) == 0, do: []
 
   defp unpack(index, pos) do
-    elem = key_for(Map.to_list(index), pos)
-
-    rest =
-      case index do
-        %{^elem => [^pos]} -> Map.delete(index, elem)
-        %{^elem => [^pos | others]} -> Map.replace!(index, elem, others)
-      end
-
-    [elem | unpack(rest, pos + 1)]
+    case key_for(Map.to_list(index), pos) do
+      :not_found -> unpack(index, pos + 1)
+      elem -> [elem | unpack(remove(index, elem, pos), pos + 1)]
+    end
   end
 
-  @doc """
-  This function uses pattern-matching smartly in its clause heads to find the first
-  element with the corresponding position, knowing that the positions are sorted.
-  And it has no base case => Let it crash!
-  """
-  # defp key_for([], pos), do: raise "position not found: #{pos}"
+  defp remove(index, elem, pos) do
+    case index do
+      %{^elem => [^pos]} -> Map.delete(index, elem)
+      %{^elem => [^pos | others]} -> Map.replace!(index, elem, others)
+    end
+  end
+
+  defp key_for([], _pos), do: :not_found
   defp key_for([{elem, [pos | _]} | _rest], pos), do: elem
   defp key_for([_ | rest], pos), do: key_for(rest, pos)
+
+  ## The following functions should not be part of the initial setup (follow plan.md)
+  def map(index, fun) do
+    index |> Map.to_list() |> map_on_list(fun) |> Map.new()
+  end
+
+  defp map_on_list([], _fun), do: []
+
+  defp map_on_list([{elem, positions} | rest], fun) do
+    [{elem, fun.(elem, positions)} | map_on_list(rest, fun)]
+  end
 end
---
+++ plan.md
@@ -0,0 +1,18 @@
+# Plan
+
+## Recursion
+- Implement BeamBA.Uniq
+    * naïvely
+    * really
+- Implement BeamBA.Index.pack
+    * naïvely
+    * really
+- Implement BeamBA.Index.unpack
+    * naïvely
+    * with bad pattern-matching on function head
+    * really
+
+## Higher order
+- Redefine BeamBA.Uniq using BeamBA.Index
+- Create BeamBA.Index.map
+- Redefine BeamBA.Uniq using Index.map
